1043452557f4f1d6bf1cf3463ac40f0fe8581e30
Nikanoras Zavadskij <nikanoras.zavadskij@mercell.com>
2023-03-01 12:35:52 +0200

    Move solution one level up

---
+++ api/src/OpenBookLibrary.Domain/Books/Book.cs
@@ -0,0 +1,18 @@
+﻿namespace OpenBookLibrary.Domain.Books;
+
+public class Book
+{
+    private readonly string[] _authors;
+
+    public Book(Isbn13 isbn13, string title, string[] authors)
+    {
+        _authors = authors;
+        Isbn13 = isbn13;
+        Title = title;
+    }
+
+    public Isbn13 Isbn13 { get; }
+    public string Title { get; }
+
+    public string[] Authors => _authors.ToArray();
+}
---
+++ api/tests/OpenBookLibrary.Domain.UnitTests/Books/BookTests.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+using OpenBookLibrary.Domain.Books;
+
+namespace OpenBookLibrary.Domain.UnitTests.Books;
+
+[TestFixture]
+public class BookTests
+{
+    [Test]
+    public void ShouldHaveTheMandatoryData()
+    {
+        var isbn13 = new Isbn13(9780132350884);
+        const string title = "Clean Code";
+        const string author = "Robert C. Martin";
+
+        var book = new Book(isbn13, title, new[] { author });
+        
+        book.Isbn13.Should().Be(isbn13);
+        book.Title.Should().Be(title);
+        book.Authors.Should().Contain(author);
+    }
+}
